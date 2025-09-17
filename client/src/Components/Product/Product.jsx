d43b6f4d200a2a5883cb053409bec9814bdf460d
biru-ka2 <bworku1221@gmail.com>
2025-09-17 23:20:54 +0300

    rating future is add for products

--- client/src/Components/Product/Product.jsx
+++ client/src/Components/Product/Product.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ProductCard from "./ProductCard";
 
 function Product() {
-  const [first, setfirst] = useState();
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-  return <div>{/* api */}</div>;
+  return (
+    <section>
+      {products.map((singleProduct) => {
+        return <ProductCard product={singleProduct} key={singleProduct.id} />;
+      })}
+    </section>
+  );
 }
 
 export default Product;
--- client/src/Components/Product/ProductCard.jsx
+++ client/src/Components/Product/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Rating from "@mui/material/Rating";
 function ProductCard() {
   return (
     <div>
@@ -8,7 +8,12 @@ function ProductCard() {
       </a>
       <div>
         <h3>title</h3>
-        <div>{/* rating */}</div>
+        <div>
+          {/* rating */}
+          <Rating value={5} precision={0.1} />
+          {/* count */}
+          <small>{60}</small>
+        </div>
         <div>{/* price */}</div>
       </div>
     </div>
