56abd8941da8724b3b94d57b6504639b6619bcd4
biru-ka2 <bworku1221@gmail.com>
2025-09-17 23:37:37 +0300

    prducts are add and fix with null value error handling

--- client/src/Components/Product/ProductCard.jsx
+++ client/src/Components/Product/ProductCard.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
-function ProductCard() {
+
+function ProductCard({ product }) {
+  const { image, title, id, rating, price } = product;
+
   return (
     <div>
-      <a href="">
-        <img src="" alt="" />
-      </a>
+      <a href="#">{image && <img src={image} alt={title} />}</a>
       <div>
-        <h3>title</h3>
+        <h3>{title}</h3>
         <div>
-          {/* rating */}
-          <Rating value={5} precision={0.1} />
-          {/* count */}
-          <small>{60}</small>
+          <Rating value={rating?.rate || 0} precision={0.1} />
+          <small>{rating?.count || 0}</small>
         </div>
         <div>
-          {/* price */}
-          <CurrencyFormat amount={75.3} />
+          <CurrencyFormat amount={price} />
         </div>
         <button>add to cart</button>
       </div>
