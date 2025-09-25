65b7cbee00b9a8eae79e9fe2a3d5640f244db1d1
biru-ka2 <bworku1221@gmail.com>
2025-09-25 20:51:04 +0300

    .

--- client/src/Components/CurrencyFormat/CurrencyFormat.jsx
+++ client/src/Components/CurrencyFormat/CurrencyFormat.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import numeral from "numeral";
 
+import classes from "./CurrencyFormat.module.css";
+
 const CurrencyFormat = ({ amount }) => {
   const formattedAmount = numeral(amount).format("$0,0.00");
   return <div>{formattedAmount}</div>;
